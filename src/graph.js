33882a15fd7834b8e5a8b3aff0b54b5d4be906c4
Juho Vepsalainen <bebraw@gmail.com>
2012-02-06 13:52:18 +0200

    Define API further

--- src/graph.js
+++ src/graph.js
@@ -1,22 +1,87 @@
 define(function() {
     return function() {
+        var set = function(eqCb) {
+            eqCb = eqCb || function(a, b) {return a == b};
+
+            return {
+                _obs: {},
+                _len: 0,
+                add: function(a) {
+                    if(this.get(a)) {
+                        return;
+                    }
+
+                    this._obs[this._len] = a;
+                    this._len++;
+                },
+                remove: function(a) {
+                    var b = this.get(a);
+
+                    if(b) {
+                        delete b;
+                        this._len--;
+                    }
+                },
+                contains: function(a) {
+                    return this.get(a)? true: false;
+                },
+                get: function(a) {
+                    if(typeof(a) == 'number') {
+                        if(a in this._obs) {
+                            return this._obs[a];
+                        }
+                    }
+                    else {
+                        for(var i = 0, len = this._len; i < len; i++) {
+                            var k = this._obs[i];
+
+                            if(eqCb(a, k)) {
+                                return a;
+                            }
+                        }
+                    }
+                },
+                length: function() {
+                    return this._len;
+                }
+            };
+        };
+
+        var links = {
+            _pairs: set(function(a, b) {
+                return a[0] == b[0] && a[1] == b[1];
+            }),
+            add: function(from, to) {
+                this._pairs.add([from, to]);
+            },
+            remove: function(from, to) {
+                this._pairs.remove([from, to]);
+            },
+            get: function(from, to) {
+                return this._pairs.get([from, to]);
+            },
+            length: function() {
+                return this._pairs.length();
+            }
+        };
+
         return {
-            nodes: [],
-            links: [],
+            nodes: set(),
+            links: links,
             length: 0,
             add: function(a) {
-                this.nodes.push(a);
+                this.nodes.add(a);
             },
             remove: function(a) {
-                this.nodes.pop();
+                this.nodes.remove(a);
             },
             link: function(from, to) {
-                if(from != to) {
-                    this.links.push([from, to]);
+                if(from != to && this.nodes.contains(from) && this.nodes.contains(to)) {
+                    this.links.add(from, to);
                 }
             },
-            unlink: function(a) {
-                this.links.pop();
+            unlink: function(from, to) {
+                this.links.remove(from, to);
             }
         };
     };
--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,7 @@
 define(['bunit', 'assert', 'graph'], function(bunit, assert, graph) {
     bunit('Graph', {
         setUp: function() {
-            return [graph(), {}, {}];
+            return [graph(), {x: 0, y: 0}, {x: 10, y: 20}, {x: -3, y: -5}];
         },
         empty: function(a) {
             this._len(a, 'nodes', 0);
@@ -11,20 +11,35 @@ define(['bunit', 'assert', 'graph'], function(bunit, assert, graph) {
             a.add(n);
             this._len(a, 'nodes', 1);
         },
+        addSame: function(a, n) {
+            this.add(a, n);
+            this.add(a, n);
+        },
         remove: function(a, n) {
             a.add(n);
             a.remove(n);
             this.empty(a);
         },
+        removeOther: function(a, n) {
+            a.remove(n);
+            this.empty(a);
+        },
         link: function(a, n1, n2) {
-            a.add(n1);
-            a.add(n2);
-            a.link(n1, n2);
+            this._link(a, n1, n2);
             this._len(a, 'links', 1);
+        },
+        linkBidi: function(a, n1, n2) {
+            this.link(a, n1, n2);
+            this._link(a, n2, n1);
+            this._len(a, 'links', 2);
         },
         linkItself: function(a, n) {
             a.add(n);
             a.link(n, n);
+            this._len(a, 'links', 0);
+        },
+        linkOther: function(a, n1, n2) {
+            a.link(n1, n2);
             this._len(a, 'links', 0);
         },
         unlink: function(a, n1, n2) {
@@ -32,12 +47,40 @@ define(['bunit', 'assert', 'graph'], function(bunit, assert, graph) {
             a.unlink(n1, n2);
             this._len(a, 'links', 0);
         },
+        unlinkInverse: function(a, n1, n2) {
+            this.link(a, n1, n2);
+            a.unlink(n2, n1);
+            this._len(a, 'links', 1);
+        },
+        unlinkBidi: function(a, n1, n2) {
+            this.linkBidi(a, n1, n2);
+            a.unlink(n1, n2);
+            this._len(a, 'links', 1);
+        },
+        unlinkOther: function(a, n1, n2, other) {
+            this.link(a, n1, n2);
+            a.unlink(n1, other);
+            this._len(a, 'links', 1);
+        },
         access: function(a, n1) {
             a.add(n1);
-            assert(a.nodes[0]).equals(n1);
+            console.log('foo', a.nodes.get(0), n1);
+            assert(a.nodes.get(0)).equals(n1);
+        },
+        getLink: function(a, n1, n2) {
+            this.link(a, n1, n2);
+            assert(a.links.get(n1, n2)).equals([n1, n2]);
+        },
+        getInvalidLink: function(a, n1, n2) {
+            assert(a.links.get(n1, n2)).not().isDefined();
+        },
+        _link: function(a, n1, n2) {
+            a.add(n1);
+            a.add(n2);
+            a.link(n1, n2);
         },
         _len: function(a, attr, val) {
-            assert(a[attr].length).equals(val || 0);
+            assert(a[attr].length()).equals(val || 0);
         }
     });
 });
