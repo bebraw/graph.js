4c31ff191ac35aca304deb2db034548606bc1027
Juho Vepsalainen <bebraw@gmail.com>
2012-02-06 10:34:18 +0200

    Add some initial API

--- src/graph.js
+++ src/graph.js
@@ -1,6 +1,22 @@
 define(function() {
-    return {
-        
+    return function() {
+        return {
+            nodes: [],
+            links: [],
+            length: 0,
+            add: function(a) {
+                this.nodes.push(a);
+            },
+            remove: function(a) {
+                this.nodes.pop();
+            },
+            link: function(from, to) {
+                this.links.push([from, to]);
+            },
+            unlink: function(a) {
+                this.links.pop();
+            }
+        };
     };
 });
 
--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,37 @@
 define(['bunit', 'assert', 'graph'], function(bunit, assert, graph) {
     bunit('Graph', {
-        empty: function() {
-            assert(false);
+        setUp: function() {
+            return [graph(), {}, {}];
+        },
+        empty: function(a) {
+            this._len(a, 'nodes', 0);
+        },
+        add: function(a, n) {
+            a.add(n);
+            this._len(a, 'nodes', 1);
+        },
+        remove: function(a, n) {
+            a.add(n);
+            a.remove(n);
+            this.empty(a);
+        },
+        link: function(a, n1, n2) {
+            a.add(n1);
+            a.add(n2);
+            a.link(n1, n2);
+            this._len(a, 'links', 1);
+        },
+        unlink: function(a) {
+            this.link(a);
+            a.unlink(a.nodes[0], a.nodes[1]);
+            this._len(a, 'links', 0);
+        },
+        access: function(a, n1) {
+            a.add(n1);
+            assert(a.nodes[0]).equals(n1);
+        },
+        _len: function(a, attr, val) {
+            assert(a[attr].length).equals(val || 0);
         }
     });
 });
