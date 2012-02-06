40247a031166f6cff68d2c18e8a8c0c16024da3f
Juho Vepsalainen <bebraw@gmail.com>
2012-02-06 19:12:44 +0200

    Separate tests to suites

--- tests/tests.js
+++ tests/tests.js
@@ -1,12 +1,59 @@
 define(['bunit', 'assert', 'graph'], function(bunit, assert, graph) {
-    bunit('Graph', {
+    var common = {
         setUp: function() {
             return [graph(), {x: 0, y: 0}, {x: 10, y: 20}, {x: -3, y: -5}];
         },
-        empty: function(a) {
+        _link: function(a, n1, n2) {
+            a.add(n1, n2);
+            a.link(n1, n2);
+        },
+        _len: function(a, attr, val) {
+            assert(a[attr].length()).equals(val || 0);
+        },
+        _empty: function(a) {
             this._len(a, 'nodes', 0);
             this._len(a, 'links', 0);
         },
+    };
+
+    function extend(a, b) {
+        var ret = {};
+
+        for(var k in a) {
+            ret[k] = a[k];
+        }
+
+        for(k in b) {
+            ret[k] = b[k];
+        }
+
+        return ret;
+    }
+
+    bunit('Graph', extend(common, {
+        empty: function(a) {
+            this._empty(a);
+        },
+        access: function(a, n1) {
+            a.add(n1);
+            assert(a.nodes.get(0)).equals(n1);
+        },
+        each: function(a, n1, n2) {
+            var is = 0;
+            var ns = 0;
+
+            a.add(n1, n2);
+            a.nodes.each(function(n, i) {
+                ns++;
+                is++;
+            });
+
+            assert(is).equals(2);
+            assert(ns).equals(2);
+        }
+    }));
+
+    bunit('Add', extend(common, {
         add: function(a, n) {
             a.add(n);
             this._len(a, 'nodes', 1);
@@ -22,27 +69,33 @@ define(['bunit', 'assert', 'graph'], function(bunit, assert, graph) {
         addSameWithMultiple: function(a, n1, n2) {
             this.add(a, n1);
             this.addMultiple(a, n1, n2);
-        },
+        }
+    }));
+
+    bunit('Remove', extend(common, {
         remove: function(a, n) {
             a.add(n);
             a.remove(n);
-            this.empty(a);
+            this._empty(a);
         },
         removeMultiple: function(a, n1, n2) {
-            this.addMultiple(a, n1, n2);
+            a.add(n1, n2);
             a.remove(n1, n2);
-            this.empty(a);
+            this._empty(a);
         },
         removeOther: function(a, n) {
             a.remove(n);
-            this.empty(a);
-        },
+            this._empty(a);
+        }
+    }));
+
+    bunit('Link', extend(common, {
         link: function(a, n1, n2) {
             this._link(a, n1, n2);
             this._len(a, 'links', 1);
         },
         linkBidi: function(a, n1, n2) {
-            this.link(a, n1, n2);
+            this._link(a, n1, n2);
             this._link(a, n2, n1);
             this._len(a, 'links', 2);
         },
@@ -55,57 +108,36 @@ define(['bunit', 'assert', 'graph'], function(bunit, assert, graph) {
             a.link(n1, n2);
             this._len(a, 'links', 0);
         },
-        unlink: function(a, n1, n2) {
-            this.link(a, n1, n2);
-            a.unlink(n1, n2);
-            this._len(a, 'links', 0);
-        },
-        unlinkInverse: function(a, n1, n2) {
-            this.link(a, n1, n2);
-            a.unlink(n2, n1);
-            this._len(a, 'links', 1);
-        },
-        unlinkBidi: function(a, n1, n2) {
-            this.linkBidi(a, n1, n2);
-            a.unlink(n1, n2);
-            this._len(a, 'links', 1);
-        },
-        unlinkOther: function(a, n1, n2, other) {
-            this.link(a, n1, n2);
-            a.unlink(n1, other);
-            this._len(a, 'links', 1);
-        },
-        access: function(a, n1) {
-            a.add(n1);
-            assert(a.nodes.get(0)).equals(n1);
-        },
         getLink: function(a, n1, n2) {
-            this.link(a, n1, n2);
+            this._link(a, n1, n2);
             assert(a.links.get(n1, n2)).equals([n1, n2]);
         },
         getInvalidLink: function(a, n1, n2) {
             assert(a.links.get(n1, n2)).not().isDefined();
         },
-        each: function(a, n1, n2) {
-            var is = 0;
-            var ns = 0;
+    }));
 
-            this.addMultiple(a, n1, n2);
-            a.nodes.each(function(n, i) {
-                ns++;
-                is++;
-            });
-
-            assert(is).equals(2);
-            assert(ns).equals(2);
+    bunit('Unlink', extend(common, {
+        unlink: function(a, n1, n2) {
+            this._link(a, n1, n2);
+            a.unlink(n1, n2);
+            this._len(a, 'links', 0);
         },
-        _link: function(a, n1, n2) {
-            a.add(n1);
-            a.add(n2);
-            a.link(n1, n2);
+        unlinkInverse: function(a, n1, n2) {
+            this._link(a, n1, n2);
+            a.unlink(n2, n1);
+            this._len(a, 'links', 1);
         },
-        _len: function(a, attr, val) {
-            assert(a[attr].length()).equals(val || 0);
+        unlinkBidi: function(a, n1, n2) {
+            this._link(a, n1, n2);
+            this._link(a, n2, n1);
+            a.unlink(n1, n2);
+            this._len(a, 'links', 1);
+        },
+        unlinkOther: function(a, n1, n2, other) {
+            this._link(a, n1, n2);
+            a.unlink(n1, other);
+            this._len(a, 'links', 1);
         }
-    });
+    }));
 });
